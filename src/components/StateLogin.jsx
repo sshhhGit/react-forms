fef163ab2ba9b43a3569598ccf3aac32fe6a7666
sshhhgit <seungho0497@hanmail.net>
2024-03-22 08:37:44 +0900

    Signup

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,17 +8,36 @@ export default function Login() {
     password: '',
   });
 
+  const [didEdit, setDidEdit] = useState({
+    eamil: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(" enteredValues ", enteredValues)
+    console.log(enteredValues);
   }
 
   function handleInputChange(identifier, value) {
     setEnteredValues(prevValues => ({
       ...prevValues,
       [identifier]: value
+    }));
+
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false,
     }))
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
   }
 
   // function handleEmailChange(event) {
@@ -40,9 +59,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur('eamil')}
             onChange={(event) => handleInputChange('eamil', event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please neter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
